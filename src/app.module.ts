6a1792888b52d5ab1010dadc9c0ead720ea37101
Venus00 <oussama@nextronic.io>
2023-06-21 16:41:54 +0100

    fix*

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppService } from './app.service';
 import { SerialModule } from './serial/serial.module';
 import { SocketModule } from './socket/socket.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [SerialModule, SocketModule, ScheduleModule.forRoot()],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'IOT_DEMO_SENSOR_BACK', 'build'),
+      exclude: ['/api/(.*)'],
+    }),
+    SerialModule,
+    SocketModule,
+    ScheduleModule.forRoot(),
+  ],
   controllers: [],
   providers: [AppService],
 })
