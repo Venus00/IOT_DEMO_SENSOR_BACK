6ab6fe3b1fa3656e5ced71c1d1fbc94a8ec19561
Venus00 <oussama@nextronic.io>
2023-06-22 13:29:17 +0100

    fix

--- src/serial/serial1.service.ts
+++ src/serial/serial1.service.ts
@@ -36,16 +36,16 @@ export class Serial1Service {
       console.log(error);
     }
 
-    // setInterval(() => {
-    //   this.fakeData();
-    // }, 500);
+    setInterval(() => {
+      this.fakeData();
+    }, 500);
   }
   // fakeData() {
   //   this.socket.send('vibration', JSON.stringify(this.payload));
   //   this.payload = [];
   // }
 
-  @Cron(CronExpression.EVERY_SECOND)
+ // @Cron(CronExpression.EVERY_SECOND)
   fakeData() {
     this.socket.send('vibration', JSON.stringify(this.payload));
     this.payload.ax = [];
